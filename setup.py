1d21bee6c76a895871525140d5cc943dd949472e
Lucas Bertola <lucasbertola@gmail.com>
2023-07-29 10:17:29 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gymnasium_connect_four',
-    version='1.1.2',
+    version='1.1.3',
     description='A connect 4 (connect four) environment for OpenAI Gym and Gymnasium',
     author='Lucas Bertola',
     url='https://github.com/lucasBertola/Connect-4-env',  
